91f04a3325a251c75edc1e51bb07e8214e00e1a1
Sufien Tout <sufientout@gmail.com>
2021-07-20 17:19:37 -0400

    Add an example test for Ordinal function

---
+++ examples_test.go
@@ -0,0 +1,13 @@
+package numeral_test
+
+import (
+	"fmt"
+
+	"github.com/suft/numeral"
+)
+
+// Example of using the Ordinal function
+func ExampleOrdinal() {
+	fmt.Println(numeral.Ordinal(1))
+	// Output: 1st <nil>
+}
